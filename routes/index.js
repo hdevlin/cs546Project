c7d2e40dd506d71c8b0acf7de37a9e87a7040b6a
bg-s-25 <hgeorgio@stevens.edu>
2020-04-15 15:50:16 -0400

    Sample lessons page with 1 row

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,10 @@ const constructorMethod = app => {
     app.get('/', (_, res) => {
       res.render('layouts/main')
     })
+
+    app.get('/lessons', (_, res) => {
+      res.render('layouts/lessons', { layout: false })
+    })
     
     app.use('*', (_, res) => {
       res.status(404).json({ error: "Not found" })
