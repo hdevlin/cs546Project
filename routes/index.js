c05bbd68d4679c02ad4fff6b0861bbbd654ab684
bg-s-25 <hgeorgio@stevens.edu>
2020-04-23 11:45:36 -0400

    initial profile layout

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const reqbody1 = require('../data/samples/reqbody_main')
 const reqbody2 = require('../data/samples/reqbody_lessons')
 
 const constructorMethod = app => {
+    // TODO pass correctly formatted request body jsons
     app.get('/', (_, res) => {
       // TODO turn lesson ids into full objects before passing to layout 
       res.render('layouts/main', { layout: false, reqbody: reqbody1 })
@@ -9,6 +10,10 @@ const constructorMethod = app => {
 
     app.get('/lessons', (_, res) => {
       res.render('layouts/lessons', { layout: false, reqbody: reqbody2 })
+    })
+
+    app.get('/profile', (_, res) => {
+      res.render('layouts/profile', { layout: false })
     })
     
     app.use('*', (_, res) => {
