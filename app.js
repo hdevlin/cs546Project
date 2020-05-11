1488989132f3650203e663054cc92476514fea7e
bg-s-25 <hgeorgio@stevens.edu>
2020-05-11 18:12:55 -0400

    started login work

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 /* global __dirname */
 const express = require("express");
 const app = express();
+const session = require('express-session');
 const static = express.static(__dirname + "/public");
-
 const configRoutes = require("./routes");
 const exphbs = require("express-handlebars");
 
@@ -14,6 +14,14 @@ app.use(express.urlencoded({ extended: true }));
 // set view engine
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
+
+// init session
+app.use(session({
+    name: 'AuthCookie',
+    secret: 'some secret string!',
+    resave: false,
+    saveUninitialized: true
+}))
 
 configRoutes(app);
 
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
+const bcrypt = require('bcrypt')
 const reqMain = require('../data/samples/reqbody_main')
 const reqLessons = require('../data/samples/reqbody_lessons')
 const reqProfile = require('../data/samples/reqbody_profile')
 const reqQuestion = require('../data/samples/reqbody_question')
+const users = require('../data/samples/users.json')
 
 const constructorMethod = app => {
     // TODO pass correctly formatted request body jsons
@@ -20,6 +22,31 @@ const constructorMethod = app => {
 
     app.get('/login', (_, res) => {
       res.render('login', { layout: false })
+    })
+
+    app.post('/login', async (req, res) => {
+      // attempt to login user with provided credentials
+      if (!req.body['username'] || !req.body['password'] || req.body['username'].trim() === '' || req.body['password'].trim() === '') {
+        res.status(401).redirect('/login/?err=ya')
+      } else {
+        // get users collection from database (maybe make a getUserByEmail function)
+        let userObj = users.find(user => user.username.toLowerCase() === req.body['username'].toLowerCase())
+        if (userObj) {
+          let passMatch = await bcrypt.compare(req.body['password'], userObj.hashedPassword)
+          .catch((err) => console.error(err))
+          if (passMatch) {
+            // create cookie, set expiration, redirect
+            let expiresIn = new Date()
+            expiresIn.setHours(expiresIn.getHours() + 1)
+            req.session.user = userObj
+            req.session.cookie.expires = expiresIn
+            res.redirect('/')
+            return
+          }
+        }
+        // access denied
+        res.status(401).redirect('login/?err=ya')
+      }
     })
 
     // temporary for debugging
