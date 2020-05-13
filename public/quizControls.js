bb27c0a0c6136cba97e7dac8ef55a8da695200c5
Edward Gomez <edwardzemog@live.com>
2020-05-13 17:37:42 -0400

    Major changes to quizControl.js and question.hbs

--- public/quizControls.js
+++ public/quizControls.js
@@ -1,66 +1,29 @@
-var questionIds = null
-var numQuestions = 0
-var selectedChoice = ''
-var correctAns = ''
-
 function setup(qids, ans) {
-    document.getElementById("q-mc-0").addEventListener("click", selectChoice)
-    document.getElementById("q-mc-1").addEventListener("click", selectChoice)
-    document.getElementById("q-mc-2").addEventListener("click", selectChoice)
-    document.getElementById("q-mc-3").addEventListener("click", selectChoice)
-    document.getElementById("prev").addEventListener("click", gotoPrev)
-    document.getElementById("next").addEventListener("click", gotoNext)
-    document.getElementById("submit").addEventListener("click", submitAnswer)
-    questionIds = qids.split(',')
-    numQuestions = questionIds.length
-    correctAns = ans
-}
+    const answer = sessionStorage.getItem("selectedAnswer");
+    let selectAnswerHTML = document.getElementById("selected-ans").innerHTML;
 
-function getCurQuestionId() {
-    let url = window.location.href.split('/')
-    return url[url.length - 1].replace('#', '')
-}
-
-function submitAnswer() {
-    if (selectedChoice == '') return
-    document.getElementById("submit").setAttribute("disabled", "")
-    if (selectedChoice == correctAns) {
-        document.getElementById("selected-ans").innerHTML = "Correct!"
-        // update db to reflect completed question
-        $.ajax({
-            type: "POST",
-            url: "/question",
-            data: {
-                completedQuestion: getCurQuestionId()
-            }
-        })
+    const questionIds = qids.split(",");
+    const correctAns = ans;
+    if (answer === correctAns) {
+        selectAnswerHTML = "Correct!";
     } else {
-        document.getElementById("selected-ans").innerHTML = `Incorrect. The right answer is ${correctAns}.`
+        selectAnswerHTML = `Incorrect. The right answer is ${correctAns}.`;
     }
+
+    let i = questionIds.findIndex((qId) => {
+        return qId == reqbody.curQuestion._id;
+    });
+
+    // Range Checking require
+    document.getElementById("next").href = `/${questionIds[i + 1]}`;
+    document.getElementById("prev").href = `/${questionIds[i - 1]}`;
 }
 
-function selectChoice() {
-    selectedChoice = $(event.target).text()
-    document.getElementById("selected-ans").innerHTML = `Selected answer: ${selectedChoice}`
-    document.getElementById("selected-ans").removeAttribute("hidden")
-}
-
-function gotoPrev() {
-    const curQId = getCurQuestionId()
-    let curQuestion = parseInt(curQId.replace("q", ""))
-    if (curQuestion > 0) {
-        curQuestion--
-        let parentUrl = window.location.href.split('/').slice(0, -1).join('/')
-        window.location.href = parentUrl + "/q" + curQuestion
-    }
-}
-
-function gotoNext() {
-    const curQId = getCurQuestionId()
-    let curQuestion = parseInt(curQId.replace("q", ""))
-    if (curQuestion < numQuestions - 1) {
-        curQuestion++
-        let parentUrl = window.location.href.split('/').slice(0, -1).join('/')
-        window.location.href = parentUrl + "/q" + curQuestion
-    }
+function selectChoice(mcNum) {
+    const selectedChoice = document.getElementById(`q-mc-${mcNum}`);
+    document.getElementById(
+        "selected-ans"
+    ).innerHTML = `Selected answer: ${selectedChoice.value}`;
+    sessionStorage.setItem("selectedAnswer", selectChoice.value);
+    document.getElementById("selected-ans").removeAttribute("hidden");
 }
