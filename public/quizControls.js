57fcc38198be890371c1faadfac1e88c02bdbb2c
Edward Gomez <edwardzemog@live.com>
2020-05-14 16:23:00 -0400

    simplify code

--- public/quizControls.js
+++ public/quizControls.js
@@ -1,39 +1,32 @@
-var questionIds = null;
-var numQuestions = 0;
-var selectedChoice = "";
-var correctAns = "";
+
+let questionIds;
+let selectedChoice = "";
+let correctAns = "";
+const currentQuestionId = window.location.pathname.replace("/question/", "");
 
 function setupQuiz(qids, ans) {
-    document.getElementById("q-mc-0").addEventListener("click", selectChoice);
-    document.getElementById("q-mc-1").addEventListener("click", selectChoice);
-    document.getElementById("q-mc-2").addEventListener("click", selectChoice);
-    document.getElementById("q-mc-3").addEventListener("click", selectChoice);
+    document.getElementsByName("multiple-choice").forEach((elem) => {
+        elem.addEventListener("click", selectChoice);
+    });
     document.getElementById("prev").addEventListener("click", gotoPrev);
     document.getElementById("next").addEventListener("click", gotoNext);
     document.getElementById("submit").addEventListener("click", submitAnswer);
     questionIds = qids.split(",");
-    numQuestions = questionIds.length;
     correctAns = ans;
 }
 
-function getCurQuestionId() {
-    let url = window.location.href.split("/");
-    console.log(url[url.length - 1].replace("#", ""));
-    return url[url.length - 1].replace("#", "");
-}
-
 function submitAnswer() {
-    if (selectedChoice == "") return;
+    if (selectedChoice === "") return;
     document.getElementById("submit").setAttribute("disabled", "");
-    if (selectedChoice == correctAns) {
+    if (selectedChoice === correctAns) {
         document.getElementById("selected-ans").innerHTML = "Correct!";
         // update db to reflect completed question
-        $.ajax({
-            type: "POST",
-            url: "/question",
-            data: {
-                completedQuestion: getCurQuestionId(),
+        fetch("/question", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
             },
+            body: JSON.stringify({ completedQuestion: currentQuestionId }),
         });
     } else {
         document.getElementById(
@@ -42,8 +35,8 @@ function submitAnswer() {
     }
 }
 
-function selectChoice() {
-    selectedChoice = $(event.target).text();
+function selectChoice(e) {
+    selectedChoice = e.target.innerHTML;
     document.getElementById(
         "selected-ans"
     ).innerHTML = `Selected answer: ${selectedChoice}`;
@@ -51,23 +44,22 @@ function selectChoice() {
 }
 
 function gotoPrev() {
-    
-    const curQId = getCurQuestionId();
+    const curQId = currentQuestionId;
     const i = questionIds.findIndex((id) => {
         return id == curQId;
     });
-    if (i != 0) {
+    if (i > 0) {
         let parentUrl = window.location.href.split("/").slice(0, -1).join("/");
         window.location.href = parentUrl + "/" + questionIds[i - 1];
     }
 }
 
 function gotoNext() {
-    const curQId = getCurQuestionId();
+    const curQId = currentQuestionId;
     const i = questionIds.findIndex((id) => {
         return id == curQId;
     });
-    if (i != questionIds.length - 1) {
+    if (i < questionIds.length - 1) {
         let parentUrl = window.location.href.split("/").slice(0, -1).join("/");
         window.location.href = parentUrl + "/" + questionIds[i + 1];
     }
