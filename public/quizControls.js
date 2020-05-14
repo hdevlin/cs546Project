e12b2658d9c135b92cfbbf09587a8ffe7fb9790a
Edward Gomez <edwardzemog@live.com>
2020-05-13 23:01:25 -0400

    should be working now

--- public/quizControls.js
+++ public/quizControls.js
@@ -1,62 +1,74 @@
-let selectedAnswer;
+var questionIds = null;
+var numQuestions = 0;
+var selectedChoice = "";
+var correctAns = "";
 
 function setup(qids, ans) {
-    // Navigation Setup
-    const curQuestionId = curQuestionId._id;
-    let questionIds;
-    if (Array.isArray(qids)) {
-        questionIds = qids;
-    } else if (typeof qids === "string") {
-        questionIds = qids.split(",");
+    document.getElementById("q-mc-0").addEventListener("click", selectChoice);
+    document.getElementById("q-mc-1").addEventListener("click", selectChoice);
+    document.getElementById("q-mc-2").addEventListener("click", selectChoice);
+    document.getElementById("q-mc-3").addEventListener("click", selectChoice);
+    document.getElementById("prev").addEventListener("click", gotoPrev);
+    document.getElementById("next").addEventListener("click", gotoNext);
+    document.getElementById("submit").addEventListener("click", submitAnswer);
+    questionIds = qids.split(",");
+    numQuestions = questionIds.length;
+    correctAns = ans;
+}
+
+function getCurQuestionId() {
+    let url = window.location.href.split("/");
+    console.log(url[url.length - 1].replace("#", ""));
+    return url[url.length - 1].replace("#", "");
+}
+
+function submitAnswer() {
+    if (selectedChoice == "") return;
+    document.getElementById("submit").setAttribute("disabled", "");
+    if (selectedChoice == correctAns) {
+        document.getElementById("selected-ans").innerHTML = "Correct!";
+        // update db to reflect completed question
+        $.ajax({
+            type: "POST",
+            url: "/question",
+            data: {
+                completedQuestion: getCurQuestionId(),
+            },
+        });
     } else {
-        console.log("qids was not an array or a string");
-        return;
-    }
-
-    let index = questionIds.findIndex((qId) => {
-        return qId == reqbody.curQuestion._id;
-    });
-
-    // // Range Checking require
-    let next = document.getElementById("next");
-    let prev = document.getElementById("prev");
-
-    if (index == 0) {
-        prev.style.display = "hidden";
-    } else {
-        prev.style.display = "";
-        prev.href = `/${questionIds[index - 1]}`;
-    }
-    if (index == questionIds.length - 1) {
-        next.style.display = "hidden";
-    } else {
-        next.href = `/${questionIds[index + 1]}`;
-        next.style.display = "";
-    }
-
-    // Text Setup
-    // This was saved when the user first selected an answer
-    // It will clear when the page closes
-    selectedAnswer = sessionStorage.getItem(`selectedAnswer${curQuestionId}`);
-
-    let selectAnswerHTML = document.getElementById("selected-ans").innerHTML;
-    if (!selectedAnswer) {
-        //nothing selected
-    } else if (selectedAnswer === ans) {
-        selectAnswerHTML = "Correct!";
-    } else {
-        selectAnswerHTML = `Incorrect. The right answer is ${ans}.`;
+        document.getElementById(
+            "selected-ans"
+        ).innerHTML = `Incorrect. The right answer is ${correctAns}.`;
     }
 }
 
-function selectChoice(mcNum) {
-    const selectedChoice = document.getElementById(`q-mc-${mcNum}`);
+function selectChoice() {
+    selectedChoice = $(event.target).text();
     document.getElementById(
         "selected-ans"
-    ).innerHTML = `Selected answer: ${selectedChoice.value}`;
-    sessionStorage.setItem(
-        `selectedAnswer${curQuestionId}`,
-        selectChoice.value
-    );
+    ).innerHTML = `Selected answer: ${selectedChoice}`;
     document.getElementById("selected-ans").removeAttribute("hidden");
+}
+
+function gotoPrev() {
+    
+    const curQId = getCurQuestionId();
+    const i = questionIds.findIndex((id) => {
+        return id == curQId;
+    });
+    if (i != 0) {
+        let parentUrl = window.location.href.split("/").slice(0, -1).join("/");
+        window.location.href = parentUrl + "/" + questionIds[i - 1];
+    }
+}
+
+function gotoNext() {
+    const curQId = getCurQuestionId();
+    const i = questionIds.findIndex((id) => {
+        return id == curQId;
+    });
+    if (i != questionIds.length - 1) {
+        let parentUrl = window.location.href.split("/").slice(0, -1).join("/");
+        window.location.href = parentUrl + "/" + questionIds[i + 1];
+    }
 }
