f8bd9bfb87357c6f90e0788cce06f37950b10b23
837220528 <44534588+837220528@users.noreply.github.com>
2020-05-14 17:38:23 -0400

    Add files via upload

--- routes/question.js
+++ routes/question.js
@@ -1,59 +1,32 @@
 const express = require("express");
-const xss = require("xss");
 const questions = require("../data/questions");
 const lessons = require("../data/lessons");
-const users = require("../data/users");
 const router = express.Router();
-const { ERRORS } = require("../data/common");
+
 router.get("/question/:id", async (req, res) => {
     // check if logged in
     if (!req.session.user) {
         res.redirect("/login");
         return;
     }
-
-    let gotQuestion;
-    try {
-        gotQuestion = await questions.getQuestion(req.params.id.toLowerCase());
-    } catch (error) {
-        if (error == ERRORS.NOEXIST) {
-            res.status(404).json({ error: "Not found" });
-            return;
-        } else {
-            console.log(`Error getting question: ${error}`);
-        }
+    const gotQuestion = await questions.getQuestion(req.params.id.toLowerCase());
+    if (!gotQuestion) {
+        res.status(404).json({ error: "Not found" });
+        return;
     }
-
-    let gotLesson;
-    try {
-        gotLesson = await lessons.getLesson(gotQuestion.lesson_id);
-    } catch (error) {
-        console.log(`Error getting Lesson: ${error}`);
-    }
-
-    const qReqBody = {
+    const gotLesson = await lessons.getLesson(gotQuestion.lesson_id);
+    let qReqBody = {
         lessonTitle: gotLesson.title,
         questions: gotLesson.questions,
         curQuestion: gotQuestion,
     };
-    res.render("question", {
-        reqbody: JSON.parse(xss(JSON.stringify(qReqBody))),
-    });
+    res.render("layouts/question", { layout: false, reqbody: qReqBody });
 });
 
 router.post("/question", async (req, res) => {
-    // update db with completed question id
-    if (!req.body.completedQuestion) return;
-    try {
-        const updatedUser = await users.updateCompletedQuestion(
-            question,
-            req.body.completedQuestion
-        );
-        req.session.user = updatedUser;
-    } catch (error) {
-        console.log(`Error updating completed question: ${error}`);
-    }
-    res.redirect("#");
+    // add completed question id to user object & update db
+    console.log(req.body);
+    res.redirect("/question");
 });
 
 module.exports = router;
