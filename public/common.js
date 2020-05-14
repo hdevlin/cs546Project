6cdd081f0f2e9757bd7bb4fc0da7b1401cecc1d8
Edward Gomez <edwardzemog@live.com>
2020-05-14 16:22:27 -0400

    use lets

--- public/common.js
+++ public/common.js
@@ -1,38 +1,39 @@
 /* Change the difficulty number on the page to a descriptive word */
 function changeDifficultyText() {
-    elems = document.getElementsByClassName("diff")
+    let elems = document.getElementsByClassName("diff");
     for (let i = 0; i < elems.length; ++i) {
         switch (elems[i].innerHTML) {
             case "1":
-                elems[i].innerHTML = "Easy"
-                break
+                elems[i].innerHTML = "Easy";
+                break;
             case "2":
-                elems[i].innerHTML = "Intermediate"
-                break
+                elems[i].innerHTML = "Intermediate";
+                break;
             case "3":
-                elems[i].innerHTML = "Advanced"
-                break
+                elems[i].innerHTML = "Advanced";
+                break;
             default:
-                elems[i].innerHTML += " difficulty"
+                elems[i].innerHTML += " difficulty";
         }
     }
 }
 
 /* Event listeners for some pages */
 function setup() {
-    elems = document.getElementsByClassName("open-lesson");
-    for (let i = 0; i < elems.length; ++i) {
-        elems[i].addEventListener("click", startClick);
+    let openElems = document.getElementsByClassName("open-lesson");
+    for (let i = 0; i < openElems.length; ++i) {
+        openElems[i].addEventListener("click", startClick);
     }
-    elems = document.getElementsByClassName("drop-lesson");
-    for (let i = 0; i < elems.length; ++i) {
-        elems[i].addEventListener("click", dropClick);
+
+    let closeElems = document.getElementsByClassName("drop-lesson");
+    for (let i = 0; i < closeElems.length; ++i) {
+        closeElems[i].addEventListener("click", dropClick);
     }
 }
 
 /* Launch lesson */
 function startClick() {
-    let parentUrl = window.location.href.split('/').slice(0, -1).join('/');
+    let parentUrl = window.location.href.split("/").slice(0, -1).join("/");
     window.location.href = parentUrl + `/lesson/${event.target.id}`;
 }
 
@@ -42,7 +43,7 @@ function dropClick() {
         type: "POST",
         url: "/lesson",
         data: {
-            dropLesson: event.target.id
-        }
-    })
+            dropLesson: event.target.id,
+        },
+    });
 }
