f33bfa1f6abc620f0d288373e87353d6c851d189
Edward Gomez <edwardzemog@live.com>
2020-05-14 13:59:24 -0400

    separated dashboard, added try-catch blocks

--- routes/index.js
+++ routes/index.js
@@ -1,35 +1,12 @@
-const xss = require("xss");
+const dashboardRoute = require("./dashboard");
 const lessonsRoute = require("./lessons");
 const questionRoute = require("./question");
 const profileRoute = require("./profile");
 const loginRoute = require("./login-logout");
 const signupRoute = require("./signup");
-const lessons = require("../data/lessons");
-const badges = require("../data/badges");
 
 const constructorMethod = (app) => {
-    app.get("/", async (req, res) => {
-        let userObj = null;
-        if (req.session.user) {
-            userObj = req.session.user;
-            for (var i in userObj['lessons']) {
-                const gotLesson = await lessons.getLesson(userObj['lessons'][i]._id.toString());
-                userObj['lessons'][i] = gotLesson;
-            }
-            for (var i in userObj['completedLessons']) {
-                const gotLesson = await lessons.getLesson(userObj['completedLessons'][i]._id.toString());
-                userObj['completedLessons'][i] = gotLesson;
-            }
-            for (var i in userObj['badges']) {
-                const gotBadge = await badges.getBadge(userObj['badges'][i]._id.toString());
-                userObj['badges'][i] = gotBadge;
-            }
-        }
-        res.render("dashboard", {
-            reqbody: JSON.parse(xss(JSON.stringify(userObj)))
-        });
-    });
-
+    app.use(dashboardRoute);
     app.use(lessonsRoute);
     app.use(profileRoute);
     app.use(loginRoute);
